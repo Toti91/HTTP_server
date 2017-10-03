016fd25d325c561200c275af59f937c6e783abe3
Toti91 <torarinng@gmail.com>
2017-10-03 16:49:03 +0000

    Getting response from server

--- src/httpd.c
+++ src/httpd.c
@@ -6,25 +6,36 @@
 #include <string.h>
 #include <ctype.h>
 #include <glib.h>
+#include <arpa/inet.h>
 
-int sockfd;
+int sockfd, connfd;
 struct sockaddr_in server, client;
+char webpage[] =
+"HTTP/1.1 200 OK\r\n"
+"Content-Type: text/html; charset=utf-8\r\n"
+"<!DOCTYPE html>\n<html>\n<head><meta charset=\"utf-8\">"
+"<title>Test page</title>\n</head>\n"
+"<body>Yeahboi</body>\n</html>";
+
+/*void closeConnection() {
+	shutdown(connfd, SHUT_RDWR);
+	close(connfd);
+}
 
 void loop() {
-
-	while(true) {
-
-	}
-}
+	
+}*/
 
 int main(int argc, char *argv[])
 {
 	if(argc != 2) {
 		printf("Usage: %s <port> \n", argv[0]);
-		return 0;
+		exit(1);
 	}
 
 	int myPort = atoi(argv[1]);
+	char payload[2048];
+	socklen_t sin_len = sizeof(client);
 
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	memset(&server, 0, sizeof(server));
@@ -46,9 +57,50 @@ int main(int argc, char *argv[])
 	}
 
 	printf("Listening on port %d \n", myPort);
-	listen(sockfd, 10);
+	listen(sockfd, 1);
 
-	loop();
+	/*while(1) {
+		printf("Loopyloop");
+		socklen_t len = (socklen_t) sizeof(client);
+		connfd = accept(sockfd, (struct sockaddr *) &client, &len);
+
+		if(connfd < 0) {
+			perror("Accepting connection failed.");
+			exit(1);
+		}
+
+		// TODO add timestamp
+		printf("timestamp : %s:%d", inet_ntoa(client.sin_addr), ntohs(client.sin_port));
+
+	}*/
+
+	while(1)
+    {
+        connfd = accept(sockfd, (struct sockaddr *) &client, &sin_len);
+
+        if(connfd == -1) {
+            perror("Connection failed...\n");
+            close(sockfd);
+            continue;
+        }
+
+        printf("Got client connection.....\n");
+        if(!fork()) {
+
+            close(sockfd);
+            memset(payload, 0, 2048);
+            read(connfd, payload, 2047);
+            printf("%s\n", payload);
+
+            write(connfd, webpage, sizeof(webpage) -1);
+            close(connfd);
+            printf("closing...\n");
+            exit(0);
+        }
+        close(connfd);
+    }
+
+	//closeConnection();
 
 	return 0;
 }
